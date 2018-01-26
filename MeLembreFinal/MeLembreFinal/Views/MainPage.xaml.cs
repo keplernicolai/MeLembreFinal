3ec1ff37bde2b27aa26fb185a15d0942862514ae
Kepler Nicolai <keplernicolai@gmail.com>
2018-01-26 13:57:38 -0200

    Primeiro Commit

---
+++ MeLembreFinal/MeLembreFinal/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace MeLembreFinal.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
