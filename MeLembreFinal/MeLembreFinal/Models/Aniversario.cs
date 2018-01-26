1193c8c2c1df352ad64051a9f0ae7447a640f6de
Kepler Nicolai <keplernicolai@gmail.com>
2018-01-26 14:58:29 -0200

    Tela Principal e Model Aniversario

---
+++ MeLembreFinal/MeLembreFinal/Models/Aniversario.cs
@@ -0,0 +1,26 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeLembreFinal.Models
+{
+    public class Aniversario : RealmObject
+    {
+        [PrimaryKey]
+        public long Id { get; set; }
+
+        public string TipoAniver { get; set; }
+        public string NomePessoa { get; set; }
+        public string Titulo { get; set; }
+        public DateTimeOffset? DataAniver { get; set; }
+        public string Detalhes { get; set; }
+        public bool SeteDias { get; set; }
+        public bool UmDia { get; set; }
+        public bool NoDia { get; set; }
+        public DateTimeOffset? DataCriacao { get; set; }
+        public string Foto { get; set; }
+    }
+}
--- MeLembreFinal/MeLembreFinal/ViewModels/MainPageViewModel.cs
+++ MeLembreFinal/MeLembreFinal/ViewModels/MainPageViewModel.cs
@@ -1,40 +1,121 @@
-﻿using Prism.Commands;
+﻿using MeLembreFinal.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
+using Realms;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace MeLembreFinal.ViewModels
 {
     public class MainPageViewModel : BindableBase, INavigationAware
     {
-        private string _title;
-        public string Title
+        public DelegateCommand NavigateCommand { get; set; }
+        private readonly INavigationService _navigationService;
+        private readonly IPageDialogService _dialogService;
+
+        private ObservableCollection<Aniversario> _listaAniversarios;
+        public ObservableCollection<Aniversario> ListaAniversarios
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            get { return _listaAniversarios; }
+            set { _listaAniversarios = value; RaisePropertyChanged(); }
         }
 
-        public MainPageViewModel()
+        private Aniversario _aniversarioSelecionado;
+        public Aniversario AniversarioSelecionado
         {
-
+            get { return _aniversarioSelecionado; }
+            set { _aniversarioSelecionado = value; RaisePropertyChanged(); ToolbarItens = true; }
         }
+
+        private bool _toolbarItens = false;
+        public bool ToolbarItens
+        {
+            get { return _toolbarItens; }
+            set { _toolbarItens = value; RaisePropertyChanged(); }
+        }
+
+        //public ICommand TouchImageButtonCommand
+        //{
+        //    get;
+        //    set;
+        //}
+
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
+        {
+            _navigationService = navigationService;
+            _dialogService = dialogService;
+            ToolbarItens = false;
+
+            EditItemCommand = new Command(Editar);
+            DeleteItemCommand = new Command(Delete);
+            // SelectedItemList = new Command(AparecerToolbarItem);
+
+            Load();
+        }
+
+        private void AparecerToolbarItem()
+        {
+            ToolbarItens = true;
+        }
+
+        private void Editar()
+        {
+            var navigationParams = new NavigationParameters();
+            navigationParams.Add("model", AniversarioSelecionado);
+            _navigationService.NavigateAsync("EditarAniversarioPage", navigationParams);
+        }
+
+        private async void Delete()
+        {
+            bool resposta = await _dialogService.DisplayAlertAsync("Deletar", "Você tem certeza que deseja apagar este item?", "Sim", "Não");
+
+            if (resposta)
+            {
+                var realmDB = Realm.GetInstance();
+                var aniversario = realmDB.All<Aniversario>().First(f => f.Id == AniversarioSelecionado.Id);
+
+                realmDB.Write(() =>
+                {
+                    realmDB.Remove(aniversario);
+                });
+
+                Load();
+            }
+        }
+
+        private void Load()
+        {
+            var realmDb = Realm.GetInstance();
+            //ListaAniversarios = realmDb.All<Aniversario>().OrderByDescending(f => f.DataCriacao).ToList();
+        }
+
+        #region Commands
+
+        public ICommand EditItemCommand { get; set; }
+        public ICommand DeleteItemCommand { get; set; }
+        public ICommand SelectedItemList { get; set; }
+
+        #endregion
 
         public void OnNavigatedFrom(NavigationParameters parameters)
         {
 
         }
 
-        public void OnNavigatedTo(NavigationParameters parameters)
-        {
-            if (parameters.ContainsKey("title"))
-                Title = (string)parameters["title"] + " and Prism";
-        }
-
         public void OnNavigatingTo(NavigationParameters parameters)
         {
 
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            
         }
     }
 }
